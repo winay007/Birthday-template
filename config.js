9154008fe63b1d8f7d5abb5ea31445c87dc5f2a0
Vinay Bhujbal <vinaybhujbal007@gmail.com>
2023-06-06 23:26:02 +0530

    first one

---
+++ config.js
@@ -0,0 +1,28 @@
+
+var config = {
+
+    texts: [
+        "Happy Birhday",      
+        "Aaaabha!!!!!!",  
+        "I know we cannot be what I want",
+        "But glad to have you as friend",
+        "Keep fooling around",
+        "Enjoyyyyy!!!!",
+        "-WINay",
+    ],
+ 
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+    },
+   
+    desc: {
+        turn_on: "Turn On Lights",
+        play: "Play Music",
+        bannar_coming: "Let's Decorate",
+        balloons_flying: "Fly With Balloons",
+        cake_fadein: "Cake for a fool",
+        light_candle: "Light Candle",
+        wish_message: "Happy Birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
